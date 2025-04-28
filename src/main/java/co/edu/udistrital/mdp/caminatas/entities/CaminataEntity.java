cd3d3b1adc901709e1dd73996c4015ac770ab638
David Ricardo Rico Moreno <ricarmor2005@gmail.com>
2025-04-27 22:47:33 -0500

    Crear entidades

---
+++ src/main/java/co/edu/udistrital/mdp/caminatas/entities/CaminataEntity.java
@@ -0,0 +1,10 @@
+package co.edu.udistrital.mdp.caminatas.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
+
+public class CaminataEntity extends BaseEntity {    
+}
