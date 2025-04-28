8473a2e054139836afdd3d70e5ce62c1ff622196
4eyesBoi <minuevacu@gmail.com>
2025-04-27 23:08:59 -0500

    Add history-receipt-user entities

---
+++ src/main/java/co/edu/udistrital/mdp/caminatas/entities/HistoryEntity.java
@@ -0,0 +1,20 @@
+package co.edu.udistrital.mdp.caminatas.entities;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Data
+@Entity
+
+public class HistoryEntity extends BaseEntity{
+
+    String place;
+
+    @Temporal(TemporalType.DATE)
+    private Date cDate;
+
+}
