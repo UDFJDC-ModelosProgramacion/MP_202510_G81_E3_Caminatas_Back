9ab8f46a8a4f107ade35c564b44e785807054291
4eyesBoi <minuevacu@gmail.com>
2025-06-08 19:44:22 -0500

    borrado de errores

--- src/main/java/co/edu/udistrital/mdp/caminatas/repositories/MultimediaRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.udistrital.mdp.caminatas.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.udistrital.mdp.caminatas.entities.MultimediaEntity;
-
-public interface MultimediaRepository extends JpaRepository<MultimediaEntity, Long> {
-    
-}
